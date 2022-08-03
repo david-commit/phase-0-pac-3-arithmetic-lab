2d3385014b82308d9d970fd1f654d067efa87c2a
david-commit <david.ondiege@students.moringaschool.com>
2022-08-03 22:18:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(){
+    1 + 80;
+}
+function subtract(){
+    100 - 1;
+}
+
+function multiply(){
+    2 * 2;
+}
+function divide(){
+    16 / 4;
+}
+function add(a, b){
+    let sum = a + b;
+    return sum;
+}
+function subtract(a, b){
+    let sub = a - b;
+    return sub;
+}
+function multiply(a, b){
+    let mul = a * b;
+    return mul;
+}
+function divide(a, b){
+    let div = a / b;
+    return div;
+}
+function increment(n){
+    let a = n + 1;
+    return a;
+}
+function decrement(n){
+    let b = n - 1;
+    return b;
+}
+function makeInt(string) {
+    let madeInt = parseInt(string, 10);
+    return madeInt;
+}
+function preserveDecimal(string){
+    let preInt = parseFloat(string);
+    return preInt;
+}
